55fb97f8753b190c328e1acd9b3fe2bd8a39ec67
Poorna-danushka <poornadanushka2@gmail.com>
2026-06-12 01:41:48 +0530

    feat(backend): define profile and user management settings routes

---
+++ server/src/routes/user.routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/user.controller');
+const { verifyToken } = require('../middlewares/auth.middleware');
+
+router.use(verifyToken);
+router.get('/me', userController.getMe);
+router.patch('/profile', userController.updateProfile);
+router.patch('/change-password', userController.changePassword);
+
+module.exports = router;
