54d790d8da23a0e22ab0dfab8a8975d35b02b256
Poorna-danushka <poornadanushka2@gmail.com>
2026-06-12 01:41:23 +0530

    feat(backend): define route for global project and task search queries

---
+++ server/src/routes/search.routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const searchController = require('../controllers/search.controller');
+const { verifyToken } = require('../middlewares/auth.middleware');
+
+router.use(verifyToken);
+router.get('/', searchController.globalSearch);
+
+module.exports = router;
